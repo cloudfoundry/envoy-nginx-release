3e46acc574068a2abae5044fc2893aa2a0cf8ce1
Arjun Sreedharan <asreedharan@pivotal.io>
2019-05-16 10:01:28 -0400

    .go: Fix cmd.exec call to nginx.exe
    Signed-off-by: Yael Harel <yharel@pivotal.io>

--- src/code.cloudfoundry.org/envoy-nginx/main.go
+++ src/code.cloudfoundry.org/envoy-nginx/main.go
@@ -7,8 +7,8 @@ import (
 )
 
 func main() {
-	fmt.Println("placeholder for source code to build an envoy faker")
-	output, err := exec.Command("./nginx.exe -c conf/nginx.conf").CombinedOutput()
+	/* Faker envoy.exe */
+	output, err := exec.Command("./nginx.exe", "-c", "conf/nginx.conf").CombinedOutput()
 	if err != nil {
 		os.Stderr.WriteString(err.Error())
 	}
